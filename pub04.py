5f345d3892e33c77a6c3b3f0a5f7e62f8e5214d6
tetsuzawa <tetsu.varmos@gmail.com>
2019-02-10 21:30:20 +0900

    mod 04

--- pub04.py
+++ pub04.py
@@ -5,20 +5,21 @@ import os
 from modules import led_flash
 from password.password import *
 
+client = mqtt.Client()
+
 def pub_test():
 
     for _i in range(0,5):
         client.publish('control', 'test')
         time.sleep(1)
 
-
-
 def pub_main():
 
-    client = mqtt.Client()
     client.tls_set('/etc/ssl/certs/ca-certificates.crt')
-    client.username_pw_set(os.environ["CLOUD_MQTT_USERNAME"], os.environ["CLOUD_MQTT_PASSWORD"])
-    client.connect(os.environ["CLOUD_MQTT_URL"], int(os.environ["CLOUD_MQTT_SSL_PORT"]), keepalive=60)
+    #client.username_pw_set(os.environ["CLOUD_MQTT_USERNAME"], os.environ["CLOUD_MQTT_PASSWORD"])
+    #client.connect(os.environ["CLOUD_MQTT_URL"], int(os.environ["CLOUD_MQTT_SSL_PORT"]), keepalive=60)
+    client.username_pw_set(CLOUD_MQTT_USERNAME, CLOUD_MQTT_PASSWORD)
+    client.connect(CLOUD_MQTT_URL, CLOUD_MQTT_SSL_PORT, keepalive=60)
 
     pub_test()
  
