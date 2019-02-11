601d8605b054f1234cf1a1b5d8c8f374d4c7c948
tetsuzawa <tetsu.varmos@gmail.com>
2019-02-11 10:51:24 +0900

    mod 12

--- line_app.py
+++ line_app.py
@@ -22,6 +22,11 @@ YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
+@app.route("/")
+def hello_world():
+    return "hello world!"
+
+
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
