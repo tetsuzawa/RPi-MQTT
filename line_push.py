a63387a52684e19a6a1b300206e04fbb63edfeda
tetsu <tetsu.varmos@gmail.com>
2019-02-16 13:58:04 +0900

    pub04>pub_line

--- line_push.py
+++ line_push.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-from modules import pub04
+from modules import pub_line
 
 from linebot import (
     LineBotApi, WebhookHandler
