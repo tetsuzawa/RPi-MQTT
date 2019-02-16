e94428a7ac21373357a2bbb40969461af176ccf2
tetsu <tetsu.varmos@gmail.com>
2019-02-16 13:53:43 +0900

    modify re

--- sub_re.py
+++ sub_re.py
@@ -37,9 +37,9 @@ def on_message(client, userdata, msg):
         led_flash.flash()
     if message == 'test0':
         led_flash.flash()
-    if message == 'yellow':
+    if message == r'yellow%s\s*':
         led_flash.flash_yellow()
-    if message == 'blue':
+    if message == r'blue%s\s*':
         led_flash.flash_blue()
 
 def sub_main():
