d2800b69fe049bc16b7870be292a861eb38e6405
tetsuzawa <tetsu.varmos@gmail.com>
2019-02-16 21:13:59 +0900

    clean

---
+++ line_app_backup.py
@@ -0,0 +1,102 @@
+from flask import Flask, request, abort
+import numpy as np
+import os
+#パブリッシャーのインポート
+from modules import pub_line
+
+#LINEBotのSDKのインポート
+from linebot import (
+    LineBotApi, WebhookHandler, exceptions
+)
+from linebot.exceptions import (
+    InvalidSignatureError, LineBotApiError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage, QuickReplyButton, QuickReply, MessageAction 
+)
+
+#Flaskのインスタンスの作成
+app = Flask(__name__)
+
+#環境変数取得
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
+YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
+
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+
+#動作確認用ウェブサイトのルーティング
+#アクセスすると hello world! を表示
+@app.route("/", methods=['GET', 'POST'])
+def hello_world():
+    return "hello world!"
+
+
+#LINEからのWebhookを処理
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: \n" + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+
+#送られてきたものがテキストメッセージのときの処理
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    
+    #送られてきたメッセージの中身の取り出し
+    text_message = event.message.text
+
+    #gpioと送られてきた場合, クイックリプライメニューを送信
+    #その他はオウム返し
+    try:
+        if text_message == 'gpio':
+            send_object = send_quick_reply_button()
+
+        else:
+            pub_line.pub_test02(text_message)
+            send_object = TextSendMessage(text=text_message)
+
+        line_bot_api.reply_message(
+            event.reply_token,
+            send_object
+        )
+
+    except exceptions.LineBotApiError as e:
+        print("start error handling")
+        print(e.status_code)
+        print(e.error.message)
+        print(e.error.details)
+        print("end")
+ 
+
+#クイックリプライメニューを送信する関数
+def send_quick_reply_button():
+
+    quick_reply_content = TextSendMessage(text = "what colors would you like?",
+                                    quick_reply = QuickReply(items = [
+                                        QuickReplyButton(action=MessageAction(label="blue", text="blue")),
+                                        QuickReplyButton(action=MessageAction(label="yellow", text="yellow"))
+                                    ]))
+
+    return quick_reply_content
+        
+
+
+#起動
+if __name__ == "__main__":
+    app.debug = True
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
--- sub.py
+++ sub.py
@@ -2,33 +2,37 @@
 import paho.mqtt.client as mqtt
 import os
 import re
+from os.path import join, dirname
+from dotenv import load_dotenv
 
-from modules import led_flash
-from password.password import *
+from modules.led_flash_class import Flash
 
-
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 
 def on_connect(client, userdata, flags, respons_code):
     print('Connected with result code '+str(respons_code))
     print('Connected to cloudMQTT')
-    #client.subscribe('isyjp/msg1st')
-    #client.subscribe('control/#')
     client.subscribe('#')
 
 def on_message(client, userdata, msg):
     message = msg.payload.decode('utf-8')
-    #print(msg.topic+' '+str(msg.payload))
     print(msg.topic+' '+message)
+    
+    blue = Flash(COUNT=5, BCM_NUM=13)
+    yellow = Flash(COUNT=5, BCM_NUM=19)
 
-    if message == re_compile('on'):
-        led_flash.flash()
-    if message == re_compile('test0'):
-        led_flash.flash()
-    if message == re_compile('yellow'):
-        led_flash.flash_yellow()
-    if message == re_compile('blue'):
-        led_flash.flash_blue()
+    if message == 'alternately':
+        yellow.flash()
+        blue.flash()
+    if message == 'yellow':
+        yellow.flash()
+    if message == 'blue':
+        blue.flash()
+
+    del blue
+    del yellow
 
 def sub_main():
     client = mqtt.Client(protocol=mqtt.MQTTv311)
@@ -36,13 +40,10 @@ def sub_main():
     client.on_message = on_message
 
     client.tls_set('/etc/ssl/certs/ca-certificates.crt')
-    client.username_pw_set(CLOUD_MQTT_USERNAME, CLOUD_MQTT_PASSWORD)
-    client.connect(CLOUD_MQTT_URL, CLOUD_MQTT_SSL_PORT, keepalive=60)
+    client.username_pw_set(os.environ["CLOUD_MQTT_USERNAME"], os.environ["CLOUD_MQTT_PASSWORD"])
+    client.connect(os.environ["CLOUD_MQTT_URL"], int(os.environ["CLOUD_MQTT_SSL_PORT"]), keepalive=60)
     client.loop_forever()
 
-def re_compile(text):
-    text = re.compile(r'%s\s*'%text, re.I)
-    return text
 
 if __name__ == '__main__':
     sub_main()
